b152c8bfd4dfb87a10d195b28c6077c8b2ce3702
Ivan Barjaktarov <ivan.thegreat@gmail.com>
2019-09-10 16:19:10 +0200

    Volumes removed from ingress script

--- infrastructure/ingress_setup.sh
+++ infrastructure/ingress_setup.sh
@@ -3,7 +3,6 @@
 
 k8_location="../k8s_objects/ingress-controller"
 k8_ingress_object=(ns-and-sa.yaml default-server-secret.yaml ingress-configmap.yaml ingress-rbac.yaml ingress-controller-daemonset.yaml)
-k8_pv_object=(db-pv.yaml www-pv.yaml)
 
 # Deploy Ingress Controller as daemonset
 echo -e "\033[1;34mDeploying Ingress Controller ...\033[0m"
